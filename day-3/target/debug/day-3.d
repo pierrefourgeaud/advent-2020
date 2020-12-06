2361dc3f84341ae307dfebccb1c65c158a665a34
Pierre Fourgeaud <contact@pierrefourgeaud.com>
2020-12-06 10:01:59 -0500

    Remove artifacts mistakenly committed

--- day-3/target/debug/day-3.d
+++
@@ -1,1 +0,0 @@
-/Users/pierrefourgeaud/Code/advent-2020/day-3/target/debug/day-3: /Users/pierrefourgeaud/Code/advent-2020/day-3/src/main.rs
--- day-3/target/debug/deps/day_3.d
+++
@@ -1,5 +0,0 @@
-/Users/pierrefourgeaud/Code/advent-2020/day-3/target/debug/deps/day_3: src/main.rs
-
-/Users/pierrefourgeaud/Code/advent-2020/day-3/target/debug/deps/day_3.d: src/main.rs
-
-src/main.rs:
